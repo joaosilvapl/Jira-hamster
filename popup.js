86aad9723f5108b81575fadf0c14aeefa9b5d41b
joaosilvapl <jhrecife@gmail.com>
2019-05-30 01:26:44 +0200

    Color cards/contextmenu on popup button click.

--- popup.js
+++ popup.js
@@ -1,15 +1,32 @@
 let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get('color', function (data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
+changeColor.onclick = function (element) {
     let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {
+                code: `var boardCards = $("div[data-issue-key]");
+                
+                if (boardCards.length > 0) {
+                    boardCards.css('background-color', 'red');
+                
+                    boardCards.contextmenu(function () {
+                        setTimeout(() => {
+                            var contextMenuOtherActionsSection = $(".aui-last");
+                
+                            if (contextMenuOtherActionsSection.length > 0) {
+                                contextMenuOtherActionsSection.css('background-color', 'blue');
+                            }
+                
+                        }, 100);
+                    });
+                }
+          `});
     });
-  };
+};
