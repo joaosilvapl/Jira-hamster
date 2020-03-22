9346574984c7ebc7dadadf19d77a745757b83750
Silva <joao.silva@dbschenker.com>
2020-03-22 16:55:50 +0100

    Saving data to sync storage (test)

--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,25 @@
+const cardInfoStoragePrefix = "cardInfo";
+
 printMessage = (message) => {
   console.log(`Jirasol: ${message}`);
 }
 
-printMessage(new Date());
+getCardInfoStorageKey = (cardId) => cardInfoStoragePrefix + cardId;
 
-printMessage(`jQuery type: ${typeof(jQuery)}`);
+var cardId1 = getCardInfoStorageKey(1);
+
+saveCardInfo = (cardId, info) => {
+  var key = cardInfoStoragePrefix + cardId;
+  chrome.storage.sync.set({ [key]: info }, () => {
+    printMessage(`Info stored for card ${cardId}: ${key} = ${info}`);
+  });
+}
+
+chrome.storage.sync.get(cardId1, (obj) => {
+  printMessage(`${cardId1} = ${obj[cardId1]}`);
+});
+
+printMessage(`jQuery type: ${typeof (jQuery)}`);
 
 if (!jQuery || !$) {
   printMessage("jQuery not loaded");
@@ -14,10 +29,17 @@ else {
 
   printMessage(`Found cards: ${cardIdElements.length}`);
 
-  var cardIds = $(cardIdElements).map((index, value) =>$(value).attr('aria-label'));
+  var cardIds = $(cardIdElements).map((index, value) => $(value).attr('aria-label'));
 
   cardIds.each((index, value) => printMessage(value));
 }
+
+var value = new Date().toString();
+
+saveCardInfo(1, value);
+
+
+
 
 
 
