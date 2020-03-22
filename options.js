e1166b9624aaac3e6dc4fdd38688488482c62965
Silva <joao.silva@dbschenker.com>
2020-03-22 22:18:45 +0100

    Options popup - save card info

--- options.js
+++ options.js
@@ -1,15 +1,29 @@
-let page = document.getElementById('buttonDiv');
-  const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-  function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-      let button = document.createElement('button');
-      button.style.backgroundColor = item;
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({color: item}, function() {
-          console.log('color is ' + item);
-        })
-      });
-      page.appendChild(button);
-    }
-  }
-  constructOptions(kButtonColors);
+const cardInfoStoragePrefix = "cardInfo";
+
+printMessage = (message) => {
+  console.log(`Jirasol: ${message}`);
+}
+
+getCardInfoStorageKey = (cardId) => cardInfoStoragePrefix + cardId;
+
+saveInfo = (key, info, callback) => {
+  chrome.storage.sync.set({ [key]: info }, () => {
+    callback(key, info);
+  });
+}
+
+saveCardInfo = (cardId, info) => {
+  var key = cardInfoStoragePrefix + cardId;
+  saveInfo(key, info, (x, y) => {
+    printMessage(`Info stored for card ${cardId}: ${x} = ${y}`);
+  });
+}
+
+saveButtonClick = () => {
+  var cardId = $("#cardId").val();
+  var cardInfo = $("#cardInfo").val();
+
+  saveCardInfo(cardId, cardInfo);
+}
+
+$("#saveButton").click(saveButtonClick);
