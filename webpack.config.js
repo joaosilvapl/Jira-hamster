c950d45087c7511721596498ac8d12e246cc1245
Joao Henrique de Almeida e Silva <jhrecife@hotmail.com>
2020-04-11 01:07:08 +0200

    Webpack generate contentscript_bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/contentscript.js',
+    mode: 'production',
+    optimization: {
+        // We no not want to minimize our code.
+        minimize: false
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/contentscript_bundle.js'
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: 'static/images', to: 'images' },
+            { from: 'static/js', to: 'js' },
+            { from: 'static/css', to: 'css' },
+            { from: 'static/popup.html', to: 'popup.html' },
+            { from: 'manifest.json', to: 'manifest.json' }
+        ])
+    ]
+};
