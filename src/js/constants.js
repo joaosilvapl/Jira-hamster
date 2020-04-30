4c231afd69a9ac8c960a365d983f83d1fd19ac86
Silva <joao.silva@dbschenker.com>
2020-04-30 19:34:27 +0200

    Added highlight commands

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,6 +3,10 @@ export const Command_LoadNotesBacklog = "load-notes-backlog";
 export const Command_ToggleFocusMode = "toggle-focus-mode";
 export const Command_ClearNotes = "clear-notes";
 
+export const Command_ToggleHighlight1 = "toggle-highlight-1";
+export const Command_ToggleHighlight2 = "toggle-highlight-2";
+export const Command_ToggleHighlight3 = "toggle-highlight-3";
+
 export const Event_SaveCardNote = "save-card-note";
 
 export const CardNoteClass_Empty = "cardNoteEmpty";
--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -37,21 +37,20 @@ class ContentScript {
 
     addPageDomElements = () => {
         $('head').append($('<link rel="stylesheet" type="text/css" />')
-        .attr('href', chrome.extension.getURL("../css/pagestyle.css")));
-    
-    $.getScript(chrome.extension.getURL("js/pagescript_bundle.js"));
-    
-    document.addEventListener(constants.Event_SaveCardNote, function (data) {
-        //Send message to the background script
-        chrome.runtime.sendMessage({ 'cardId': data.detail.cardId, 'cardNote': data.detail.cardNote });
-    });
+            .attr('href', chrome.extension.getURL("../css/pagestyle.css")));
+
+        $.getScript(chrome.extension.getURL("js/pagescript_bundle.js"));
+
+        document.addEventListener(constants.Event_SaveCardNote, function(data) {
+            //Send message to the background script
+            chrome.runtime.sendMessage({ 'cardId': data.detail.cardId, 'cardNote': data.detail.cardNote });
+        });
     }
 
     toggleVisibility = (elementId) => {
         if ($(`#${elementId}:visible`).length) {
             $(`#${elementId}`).hide();
-        }
-        else {
+        } else {
             $(`#${elementId}`).show();
         }
     }
@@ -60,6 +59,19 @@ class ContentScript {
         this.toggleVisibility('ghx-header');
         this.toggleVisibility('navigation-app');
         this.toggleVisibility('ghx-operations');
+    }
+
+    toggleHighlight = (index) => {
+        var elements = document.querySelectorAll('.ghx-backlog-card:hover[data-issue-id], .ghx-issue:hover[data-issue-id]');
+        //var elements = document.querySelectorAll('.ghx-issue:hover[data-issue-id]');
+
+        this.logger.logMessage(`Hover: ${elements.length}`);
+
+        $(elements).removeClass(`cardHighlight1`);
+        $(elements).removeClass(`cardHighlight2`);
+        $(elements).removeClass(`cardHighlight3`);
+
+        $(elements).addClass(`cardHighlight${index}`);
     }
 
     messageReceived = (message) => {
@@ -75,6 +87,15 @@ class ContentScript {
             case constants.Command_ToggleFocusMode:
                 this.toggleFocusMode();
                 break;
+            case constants.Command_ToggleHighlight1:
+                this.toggleHighlight(1);
+                break;
+            case constants.Command_ToggleHighlight2:
+                this.toggleHighlight(2);
+                break;
+            case constants.Command_ToggleHighlight3:
+                this.toggleHighlight(3);
+                break;
             case constants.Command_ClearNotes:
                 this.cardInfoRepository.clearAllNotes();
                 break;
@@ -85,4 +106,8 @@ class ContentScript {
     }
 }
 
-chrome.runtime.onMessage.addListener(new ContentScript().messageReceived);
+let contentScript = new ContentScript();
+
+contentScript.addPageDomElements();
+
+chrome.runtime.onMessage.addListener(contentScript.messageReceived);
