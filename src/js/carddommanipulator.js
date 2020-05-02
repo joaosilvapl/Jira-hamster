5db86808708b3d694cad0d44a9feadc31c39bfea
Silva <joao.silva@dbschenker.com>
2020-05-02 19:28:19 +0200

    joaosilvapl

--- src/js/carddommanipulator.js
+++ src/js/carddommanipulator.js
@@ -22,9 +22,9 @@ export default class CardDomManipulator {
         return cardsInfo;
     }
 
-    getCardElement = (cardId, note) => {
-        var noteText;
-        var noteClass;
+    buildCardElement = (cardId, note, isBacklogMode) => {
+        let noteText;
+        let noteClass;
         if (note && note != '') {
             noteText = note;
             noteClass = "";
@@ -33,22 +33,47 @@ export default class CardDomManipulator {
             noteClass = constants.CardNoteClass_Empty;
         }
 
+        if (isBacklogMode) {
+            noteClass += " ghx-summary"
+        }
+
         return `<div id="cardDiv-${cardId}"
-                 class='cardNote ${noteClass}' onclick='event.stopPropagation();showModal("${cardId}");return false;'>${noteText}</div>`;
+                 class='cardNote ${noteClass}' 
+                 onclick='event.stopPropagation();showModal("${cardId}");return false;'>
+                 ${noteText}
+                 </div>`;
     }
 
     appendNoteToCard = (cardId, note, isBacklogMode) => {
 
         let cardSelectorClass = isBacklogMode ? "ghx-backlog-card" : "ghx-issue";
 
-        var cardDiv = $(`.${cardSelectorClass}[data-issue-key='${cardId}']`);
+        let cardDivs = $(`.${cardSelectorClass}[data-issue-key='${cardId}']`);
 
-        if (cardDiv.length == 0) {
+        if (cardDivs.length == 0) {
             this.logger.logMessage(`Error: Could not find card for id=${cardId}`);
         } else {
+
+            let cardDiv = $(cardDivs[0]);
+
             this.logger.logMessage(`Adding note div for id=${cardId}`);
-            var cardElement = this.getCardElement(cardId, note);
-            $(cardDiv).append(cardElement);
+            var cardElement = this.buildCardElement(cardId, note, isBacklogMode);
+
+            if (isBacklogMode) {
+                let cardRowElements = cardDiv.find('div.ghx-row.ghx-plan-main-fields');
+
+                if (cardRowElements.length == 0) {
+                    this.logger.logMessage(`Error: Could not find card row for id=${cardId}`);
+                } else {
+                    let cardRowElement = $(cardRowElements[0]);
+
+                    cardRowElement.append('<div class="ghx-backlog-card-expander-spacer"></div>');
+                    cardRowElement.append(cardElement);
+                }
+
+            } else {
+                cardDiv.append(cardElement);
+            }
         }
 
     }
