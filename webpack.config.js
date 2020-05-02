f915a1a07953ae89f0e8888bea71ec54b745d37a
Silva <joao.silva@dbschenker.com>
2020-05-02 20:17:28 +0200

    Installed material design textfield and button

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         pagescript: './src/js/pagescript.js',
     },
     mode: 'production',
-    optimization: { //TODO: make this with conditional
+    optimization: { //TODO: For now we want to be able to easily debug, thus we avoid minifying
         minimize: false
     },
     output: {
@@ -20,21 +20,21 @@ module.exports = {
         new CopyPlugin([
             { from: 'static/images', to: 'images' },
             { from: 'static/js', to: 'js' },
-            { from: 'static/css', to: 'css' },
+            { from: 'static/css/pagestyle.css', to: 'css/pagestyle.css' },
+            { from: 'static/css/popup.css', to: 'css/popup.css' },
+            { from: 'static/popup.html', to: 'popup.html' },
             { from: 'static/popup.html', to: 'popup.html' },
             { from: 'manifest.json', to: 'manifest.json' }
         ])
     ],
     module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                options: {
-                    plugins: ['transform-class-properties']
-                }
-            },
-        ]
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader',
+            options: {
+                plugins: ['transform-class-properties']
+            }
+        }, ]
     }
 };
